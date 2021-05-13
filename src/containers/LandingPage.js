d78051f5bcb76b0a8ed7dd7d850b4310f26c508e
ishita <ishita.vig@outlook.com>
2021-05-13 14:50:56 +1000

    WIP adding method and UI for restaurant search

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../assets/images/logo.png";
+import { Button, TextField } from "@material-ui/core";
+import axios from "axios";
 
 const LandingPage = (props) => {
+  const [place, setPlace] = useState("");
+  const [restaurantResults, setRestaurantResults] = useState([]);
+
+  const searchRestaurants = async () => {
+    setRestaurantResults([]);
+    const result = await axios.post(
+      `http://localhost:9000/users/search-restaurants?city=${place}`
+    );
+    setRestaurantResults(result.data);
+  };
+
   return (
     <div className="text-center">
       <img src={Logo} width="20%" alt="logo" />
+      <div className="col-12 text-center">
+        <TextField value={place} onChange={(e) => setPlace(e.target.value)} />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => searchRestaurants()}
+        >
+          Search
+        </Button>
+      </div>
+      <div>
+        {restaurantResults &&
+          restaurantResults.map((restaurant) => <div>{restaurant.name}</div>)}
+      </div>
     </div>
   );
 };
